38dd389c7a56f5895df25164b8df1478d52db01e
Sk Saddam Hosan <sksddmhosan@gmail.com>
2017-11-19 09:16:52 +0530

    sucessfully create project

---
+++ touchcorebackend/src/main/java/com/touchcor/touchcorebackend/App.java
@@ -0,0 +1,13 @@
+package com.touchcor.touchcorebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
